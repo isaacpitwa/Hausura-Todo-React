a2b1610c947b461490225f188b972d52f3bcb21d
Isaac Pitwa <isaacpitwa256@gmail.com>
2020-08-18 03:22:23 +0300

    Delete todo

--- app-boilerplate/src/components/Todo/TodoItem.js
+++ app-boilerplate/src/components/Todo/TodoItem.js
@@ -5,6 +5,18 @@ const TodoItem = ({ index, todo }) => {
   const removeTodo = e => {
     e.preventDefault();
     e.stopPropagation();
+    removeTodoMutation({
+      variables: { id: todo.id },
+      optimisticResponse: true,
+      update: cache => {
+        const existingTodos = cache.readQuery({ query: GET_MY_TODOS });
+        const newTodos = existingTodos.todos.filter(t => t.id !== todo.id);
+        cache.writeQuery({
+          query: GET_MY_TODOS,
+          data: { todos: newTodos }
+        });
+      }
+    });
   };
   const TOGGLE_TODO = gql`
     mutation toggleTodo($id: Int!, $isCompleted: Boolean!) {
@@ -18,13 +30,13 @@ const TodoItem = ({ index, todo }) => {
   `;
 
   const REMOVE_TODO = gql`
-  mutation removeTodo ($id: Int!) {
-    delete_todos(where: {id: {_eq: $id}}) {
-      affected_rows
+    mutation removeTodo($id: Int!) {
+      delete_todos(where: { id: { _eq: $id } }) {
+        affected_rows
+      }
     }
-  }
-`;
-const [removeTodoMutation] = useMutation(REMOVE_TODO);
+  `;
+  const [removeTodoMutation] = useMutation(REMOVE_TODO);
 
   const toggleTodo = () => {
     toggleTodoMutation({
